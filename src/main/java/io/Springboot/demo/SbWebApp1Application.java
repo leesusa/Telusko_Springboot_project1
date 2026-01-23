4424f7eb03bebddcb11f33817004ba1a24b34c5b
Susan Lee <susan1998_lee312@hotmail.com>
2026-01-23 12:29:45 -0700

    first commit

---
+++ src/main/java/io/Springboot/demo/SbWebApp1Application.java
@@ -0,0 +1,13 @@
+package io.Springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbWebApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbWebApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/Springboot/demo/SbWebApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.Springboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbWebApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
